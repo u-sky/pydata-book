8157b65aa648b30f909927b53e53ed27af004b1e
yusuke0324 <take.yusuke@gmail.com>
2017-03-07 21:23:23 +0900

    chap03

---
+++ ipython_log.py
@@ -0,0 +1,8 @@
+# IPython log file
+
+get_ipython().magic(u'pinfo %gui')
+get_ipython().magic(u'logstart')
+get_ipython().system(u'cmd')
+get_ipython().magic(u'pwd')
+get_ipython().system(u'python')
+0 s
